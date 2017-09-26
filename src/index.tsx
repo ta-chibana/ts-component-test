62b112c827143a541a6f0d0e93ddc2cd573c514c
ta-chibana <tachibana.gmc@gmail.com>
2017-09-27 01:11:57 +0900

    Modify src/index.ts

--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import MyComponent from './MyComponent'
-
-ReactDom.render(<MyComponent content="hello world"/>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.tsx'),
+  entry: path.resolve(__dirname, 'src/index.ts'),
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'index.js'
